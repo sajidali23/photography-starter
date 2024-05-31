62b499e2747e0eb5954a4364160eb9b0ef6eecbf
sajidali23 <sajidalix20@gmail.com>
2024-05-31 10:32:06 +0530

    comitted

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import WomanImg from '../img/about/woman.png';
+import {Link} from 'react-router-dom';
 
 const About = () => {
-  return <div>About</div>;
+  return <section className='section'>
+    <div className='container mx-auto h-full relative'>
+      <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16'>
+        <div className='flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow:hidden'>
+          <img src={WomanImg}/>
+          </div>
+        <div className='flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start'>
+          <h1 className='h1'>About Me</h1>
+          <p className='mb-12 max-w-s,'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum <b>has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley </b> of type and scrambled it to make a type specimen book.
+             It has survived not only five centuries, but also the leap into electronic typesetting, 
+
+          <br />
+          <br />    
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          </p>
+          <Link to={'/portfolio'} className='btn'>View My Services</Link>
+        </div>
+      </div>
+    </div>
+  </section>;
 };
 
 export default About;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import Image1 from '../img/portfolio/1.png';
+import Image2 from '../img/portfolio/2.png';
+import Image3 from '../img/portfolio/3.png';
+import Image4 from '../img/portfolio/4.png';
+import {Link} from 'react-router-dom';
+
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
-};
+  return <section className='section'>
+    <div className='container mx-auto h-full relative'>
+      <div className='flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 lg:pt-36 pb-8'>
+        <div className='lex flex-col lg:items-start'>
+          <h1 className='h1'>Services</h1>
+          <p className='mb-12 max-w-s,'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum <b>has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley </b> of type and scrambled it to make a type specimen book.
+             It has survived not only five centuries, but also the leap into electronic typesetting, 
 
+          <br />
+          <br />    
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          </p>
+          <Link to={'/contact'} className='btn mb-[30px] mx-auto lg:mx-0'>Hire me</Link>
+        </div>
+        <div>
+          <div className='max-w-[250px] lg:max-w-[320px] h-[187px] lg:h-[220px]'>
+            <img src={Image1} alt=''/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>;
+}
 export default Portfolio;
